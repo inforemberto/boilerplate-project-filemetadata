a4046c0d0aef2be2a38ba29c4bd247f6b3aa0751
inforemberto <>
2021-06-03 12:57:18 +0000

    file metadata

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var cors = require('cors');
+var multer = require('multer');
 require('dotenv').config()
 
 var app = express();
+var upload = multer({dest: 'uploads/'});
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,7 +13,13 @@ app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res, next) {
+  return res.json({
+    "name":req.file.originalname,
+    "type":req.file.mimetype,
+    "size":req.file.size
+  })
+})
 
 
 const port = process.env.PORT || 3000;
